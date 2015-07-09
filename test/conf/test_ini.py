5e255ac62c43d1b0689983d8533eafbd8e2dc0d0
Rickard <rickard.qvist@geomatikk.se>
2015-07-09 23:40:01 +0200

    Fixat typo i earth_world.ini. Skapat tester för att se att ländernas geografi stämmer.

--- test/conf/test_ini.py
+++ test/conf/test_ini.py
@@ -4,6 +4,7 @@ import unittest
 import configobj
 from validate import Validator
 import os
+import test_connections
 
 #[[iceland]]
 #start_soldiers = 1
@@ -18,9 +19,17 @@ class IniTestCase(unittest.TestCase):
             conf = configobj.ConfigObj('../../conf/earth_world.ini',
                                        configspec='../../conf/board_spec.ini')
         val = conf.validate(validator)
-        print(val)
         self.assertTrue(val)
 
+    def test_geography(self):
+        config = { 'continent1':{
+                        'country1':{'connections':['country2','country3']},
+                        'country2':{'connections':['country1','country3']},
+                        'country3':{'connections':['country1','country2']},
+                        'country4':{'connections':['country5','country2']}
+                            },
+        }
 
+        self.assertTrue(test_connections.test_connections(config))
 if __name__ == '__main__':
     unittest.main()
